27e088eeb5e40a5420093001f0ab9c2fab0b4089
Shohan <ashraful.mallik100@gmail.com>
2019-09-04 23:12:59 -0400

    fixed minor formatting stuff

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React,{Component} from 'react';
-import './App.css';
-import {Cardlist} from './components/card-list/card-list.component'
-import {SearchBox} from './components/search-box/search-box.component'
+import React, { Component } from "react";
+import "./App.css";
+import { Cardlist } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-class App extends Component { // using Class Component gives us access to state. What is state? JS object with properties that we can access at any point in our class.
-// API:  
-//Lifecycle Methods: Methods that get called at different states automatically by React when this component gets rendered.
+class App extends Component {
+  // using Class Component gives us access to state. What is state? JS object with properties that we can access at any point in our class.
+  // API:
+  //Lifecycle Methods: Methods that get called at different states automatically by React when this component gets rendered.
 
-  constructor(){
+  constructor() {
     super(); //extending on functionality of components from react
 
-    this.state={
-      characters: [
-     
-      ]
-      ,
-      searchField: ''
-    }
+    this.state = {
+      characters: [],
+      searchField: ""
+    };
 
     //handleChange = this.handleChange.bind(this);
-    // this.setState doesn't set the context without binding, which we want to connect to our App conmponent.
-    //this.setState lets us modify the state object on every single html element. 
+    // this.setState doesn't actually set the context without binding, which we want to connect to our App conmponent.
+    //this.setState lets us modify the state object on every single html element.
 
     //binding is tedious, and we can avoid it using ES6 ARROW FUNCTIONS~!
   }
 
   componentDidMount() {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
-    .then(response => response.json())
-    .then(users => this.setState({characters:users.results})) //returns a promise 
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => response.json())
+      .then(users => this.setState({ characters: users.results })); //returns a promise
   }
   //when this component mounts, it calls whatever block of code we write.
 
@@ -36,19 +34,22 @@ class App extends Component { // using Class Component gives us access to state.
   //   this.setState({searchField: e.target.value})
   // }
 
-  handleChange = (e) => { // Arrow function automatically sets the context based on code its given, and this is seen as an arrow function and it automatically binds this. 
-    this.setState({searchField: e.target.value})
-  }
+  handleChange = e => {
+    // Arrow function automatically sets the context based on code its given, and this is seen as an arrow function and it automatically binds this.
+    this.setState({ searchField: e.target.value }); //lexical scoping
+  };
 
-  render(){
-    // destructuring - pull properties off objects and set them to constants that we can put somewhere eslse 
-  const {characters, searchField } = this.state; // this is set to the context of our class component, its within the scope so knows that we are asking for the state of 'this' class component.
-  const filteredCharacters= characters.filter(character => character.name.toLowerCase().includes(searchField.toLowerCase()))
+  render() {
+    // destructuring - pull properties off objects and set them to constants that we can put somewhere eslse
+    const { characters, searchField } = this.state; // this is set to the context of our class component, its within the scope so knows that we are asking for the state of 'this' class component.
+    const filteredCharacters = characters.filter(character =>
+      character.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     //above line is equal to const characters = this.state.characters and const searchField = this.state.searchField
     //includes checks string value passed is actually in the string that its being called on
-  return (
-    <div className="App">
-   {/* } <input 
+    return (
+      <div className="App">
+        {/* } <input 
       type='search' 
       placeholder='Search Cats' 
       onChange={e => this.setState({ // setState is an asynchronous function call, so set state doesnt happen immediately
@@ -58,12 +59,12 @@ class App extends Component { // using Class Component gives us access to state.
      
     }, () => console.log(e))
    
-  }/> {/* gives us a placeholder*/} 
-  <SearchBox 
-  placeholder='Search Pokemon' 
-  handleChange = {
-    this.handleChange
- /* {e => this.setState({ // setState is an asynchronous function call, so set state doesnt happen immediately
+  }/> {/* gives us a placeholder*/}
+        <SearchBox
+          placeholder="Search Pokemon"
+          handleChange={
+            this.handleChange
+            /* {e => this.setState({ // setState is an asynchronous function call, so set state doesnt happen immediately
       //console.log is synchronous
       //so to log our state, we have to pass it as a second argument to setState (below)
       searchField: e.target.value
@@ -71,20 +72,20 @@ class App extends Component { // using Class Component gives us access to state.
     }, () => console.log(e))
    
  */
-    
-   } />
-    <Cardlist characters = {filteredCharacters} > { /*//component recieves state as prop.  */ }
-    
-    {
-      //props.characters.map( character => 
-      //<h1 key={character.id}> {character.name}</h1>
-     // ) 
-     //gets styling instantly because of the props.children property in the component file which the map functions gets passed into.
-    }
-    </Cardlist>
-            
-  </div>
-    )
+          }
+        />
+        <Cardlist characters={filteredCharacters}>
+          {" "}
+          {/*//component recieves state as prop.  */}
+          {
+            //props.characters.map( character =>
+            //<h1 key={character.id}> {character.name}</h1>
+            // )
+            //gets styling instantly because of the props.children property in the component file which the map functions gets passed into.
+          }
+        </Cardlist>
+      </div>
+    );
   }
 }
 
